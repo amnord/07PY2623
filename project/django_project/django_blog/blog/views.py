edb12eb036834b4f6ee195adb9ed6db52e2fc124
amnord <v.amnord@gmail.com>
2023-08-29 11:27:35 -0400

    templates

--- project/django_project/django_blog/blog/views.py
+++ project/django_project/django_blog/blog/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+	{
+		'author': 'John',
+		'title': 'Blog 1',
+		'content': 'First Blog Content',
+		'date_posted': 'August 29, 2023'
+	},
+	{
+		'author': 'Tom',
+		'title': 'Blog 2',
+		'content': 'Second Blog Content',
+		'date_posted': 'August 30, 2023'
+	}
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog about</h1>')
+	return render(request, 'blog/about.html', {'title':'About'})
