509776be1f21148bdd8a4235ef0bd3d2a5b067c8
amnord <v.amnord@gmail.com>
2023-09-05 11:25:51 -0400

    Database

--- project/django_project/django_blog/blog/views.py
+++ project/django_project/django_blog/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Post
 
 # Create your views here.
 
-posts = [
-	{
-		'author': 'John',
-		'title': 'Blog 1',
-		'content': 'First Blog Content',
-		'date_posted': 'August 29, 2023'
-	},
-	{
-		'author': 'Tom',
-		'title': 'Blog 2',
-		'content': 'Second Blog Content',
-		'date_posted': 'August 30, 2023'
-	}
-]
-
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
